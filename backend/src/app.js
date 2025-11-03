b26668719e1be03ac239e7d3e5f8f858f6ef2a0b
= <vinaykumarreddycse.com>
2025-11-03 15:46:38 +0530

    changed cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const reviewRouter = require("./routes/review") 
 const cors = require("cors")
 app.use(cors({
-     origin: ['http://localhost:3000', 'http://localhost:5173'],
+     origin:true,
     credentials: true  // Allow cookies
 }));
 app.use(cookieParser())
