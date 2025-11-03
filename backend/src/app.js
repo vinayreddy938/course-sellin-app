9f3b39409fc2ff8b159e2e983a1c311b6acf53da
= <vinaykumarreddycse.com>
2025-11-03 15:39:18 +0530

    changed cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const reviewRouter = require("./routes/review") 
 const cors = require("cors")
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+     origin: ['http://localhost:3000', 'http://localhost:5173'],
     credentials: true  // Allow cookies
 }));
 app.use(cookieParser())
